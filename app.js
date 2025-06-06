eac520f11f4148e97bc6906426c6435b65f7b974
madhumitaaa <madhumita.mandal.ee26@heritageit.edu.in>
2025-06-06 12:30:05 +0530

    added javascript file and modified the css

---
+++ app.js
@@ -0,0 +1,20 @@
+let btn=document.querySelectorAll(".im");
+     btn.forEach((btn) => {
+         btn.addEventListener('click', () => {
+         alert('This feature is under development!');
+     })
+     });
+ let get=document.querySelectorAll(".get");
+     get.forEach((get) => {
+         get.addEventListener('click', () => {
+         alert('we will lead you to the next page !');
+     })
+     });
+     let k=document.querySelector(".k");
+         get.addEventListener('click', () => {
+         alert('we will lead you to the next page !');
+     });
+    
+     
+     
+    
